bf0a38ac704dc753ac657a828495ed6cb4569c6b
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-02-29 00:36:50 -0500

    Add react-router-dom and create layout and login components

---
+++ Flashcard-App/src/components/login.jsx
@@ -0,0 +1,15 @@
+
+export const Login = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>FlashCard</h1>
+        </div>
+        <div>
+          <button className="btn btn-active">Sign in with Google</button>
+        </div>
+      </div>
+    </>
+  )
+};
