913aacb3a2f6f53ed8cafe1a277854d420fdfaa6
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-03-30 20:19:01 -0400

    Add @radix-ui/react-slot and Button component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
   return (
-    <div>
-      <Card></Card>
+    <div className="flex items-center justify-items-center justify-center h-screen">
+      <Card className="h-40 flex flex-col justify-center items-center p-10">
+        <p className="text-3xl pb-2 align-text-top">Flashcard App</p>
+        <Button>Sign in with Google</Button>
+      </Card>
     </div>
   )
 }
