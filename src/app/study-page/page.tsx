10c1eb8eb868d712c0277ac78b1d98024c19895b
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-10 18:51:16 -0400

    Refactor study page component to use an array of card sets

--- src/app/study-page/page.tsx
+++ src/app/study-page/page.tsx
@@ -1,30 +1,54 @@
+"use client";
+
 import Navbar from "@/components/navbar"
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function SetPage() {
-  const card_set = {
-    "Ejemplo":"Example",
-    "Silla":"Chair",
-    "Sencillo":"Simple",
-    "Lavar":"To wash",
-    "Creer":"To believe",
-    "Tener razón":"To be right"
+  const card_set = [
+    ["Ejemplo","Example"],
+    ["Silla","Chair"],
+    ["Sencillo","Simple"],
+    ["Lavar","To wash"],
+    ["Creer","To believe"],
+    ["Tener razón","To be right"]
+  ];
+
+  const [showFront, setShowFront] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [complete, setComplete] = useState(false);
+
+  const onFlip = () => {
+    setShowFront(!showFront);
   }
+
+  const nextCard = () => {
+    if (currentIndex < card_set.length - 1) {
+      setTimeout(()=> {
+        setShowFront(true);
+        setCurrentIndex(currentIndex + 1);
+      }, 100);
+    } else {
+      setComplete(true);
+    }
+  }
+
   return (
     <div className="h-screen">
       <Navbar />
       <div className="container flex flex-col items-center justify-center py-20 h-max">
-        <Card className="w-[700px] h-[400px] flex items-center justify-center">
-          <p className="text-4xl">Ejemplo</p>
+        <p className="pb-4">{currentIndex + 1} / {card_set.length}</p>
+        <Card className="w-[700px] h-[400px] flex items-center justify-center" onClick={onFlip}>
+          {showFront && <p className="text-4xl">{card_set[currentIndex][0]}</p>}
+          {!showFront && <p className="text-4xl">{card_set[currentIndex][1]}</p>}
         </Card>
-        <Card className="w-[700px] h-[400px] flex items-center justify-center">
-          <p className="text-4xl">Example</p>
-        </Card>
-        <div>
-          <Button variant="ghost">←</Button>
-          <Button variant="ghost">→</Button>
+        {!showFront &&
+        <div className="pt-4 flex gap-2">       
+          <Button variant="ghost" onClick={nextCard}>Correct</Button>
+          <Button variant="ghost" onClick={nextCard}>Wrong</Button>
         </div>
+        }
       </div>
     </div>
   )
