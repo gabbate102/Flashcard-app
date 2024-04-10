0a68b0db8f7219a30427149c8a0910f0c1143c38
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-10 17:01:47 -0400

    Added sample data to study page

--- src/app/study-page/page.tsx
+++ src/app/study-page/page.tsx
@@ -3,12 +3,23 @@ import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 export default function SetPage() {
+  const card_set = {
+    "Ejemplo":"Example",
+    "Silla":"Chair",
+    "Sencillo":"Simple",
+    "Lavar":"To wash",
+    "Creer":"To believe",
+    "Tener razón":"To be right"
+  }
   return (
     <div className="h-screen">
       <Navbar />
       <div className="container flex flex-col items-center justify-center py-20 h-max">
         <Card className="w-[700px] h-[400px] flex items-center justify-center">
           <p className="text-4xl">Ejemplo</p>
+        </Card>
+        <Card className="w-[700px] h-[400px] flex items-center justify-center">
+          <p className="text-4xl">Example</p>
         </Card>
         <div>
           <Button variant="ghost">←</Button>
