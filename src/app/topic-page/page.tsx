8083f6eda6b30c892b866b15afea60128c078679
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-10 19:52:17 -0400

    Update topic page component to display search parameter

--- src/app/topic-page/page.tsx
+++ src/app/topic-page/page.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-export default function TopicPage() {
+export default function TopicPage({searchParams}: { searchParams: { topic: string } }) {
   const [topic, setTopic] = useState();
   return (
     <>
       <Navbar />
       <div className="container flex flex-col items-center">
         <div className="w-1/2 py-8 flex justify-between items-center">
-          <p className="text-2xl">Spanish</p>
+          <p className="text-2xl">{searchParams.topic}</p>
           <Link href={`/edit-page?topic=${topic}`}><Button>Edit</Button></Link>
         </div>
         <div className="flex flex-col w-1/2 my-auto gap-4">
