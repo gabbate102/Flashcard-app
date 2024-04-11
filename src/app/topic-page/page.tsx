6f7b87cac2cbe84a8f25930e7973ce12b8c0253b
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-10 21:21:23 -0400

    Fix link in topic page component

--- src/app/topic-page/page.tsx
+++ src/app/topic-page/page.tsx
@@ -16,7 +16,7 @@ export default function TopicPage({searchParams}: { searchParams: { topic: strin
           <Link href={`/edit-page?topic=${topic}`}><Button>Edit</Button></Link>
         </div>
         <div className="flex flex-col w-1/2 my-auto gap-4">
-          <p className="text-xl"><Link href={`/set-page?set=All`}>All Vocabulary</Link></p>
+          <p className="text-xl"><Link href={`/set-page?topic=set=All`}>All Vocabulary</Link></p>
           <p className="text-xl"><Link href={`/set-page?set=New`}>New</Link></p>
         </div>
       </div>
